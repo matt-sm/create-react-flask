00b33518ebf405f039e1ddf835d2baed3a0fccb2
Matt <matt.smith@digital.gov.au>
2018-01-02 14:34:15 +1100

    add redux store

--- client/src/index.js
+++ client/src/index.js
@@ -2,5 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { App } from './App';
+import { Provider } from 'react-redux';
+import { homeReducer } from './reducers';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+    homeReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    applyMiddleware(thunk)
+  );
+
+ReactDOM.render(
+    <Provider store={store}>
+      <App />
+  </Provider>, 
+  document.getElementById('root')
+);
---
+++ client/src/reducers/index.js
@@ -0,0 +1,38 @@
+import { CHANGE_FORM, SET_AUTH, SENDING_REQUEST, SET_ERROR_MESSAGE } from 'constants/AppConstants';
+
+const initialState = {
+  formState: {
+    username: '',
+    password: ''
+  },
+  currentlySending: false,
+  loggedIn: false,
+  errorMessage: ''
+};
+
+export function homeReducer(state = initialState, action) {
+  switch (action.type) {
+    case CHANGE_FORM:
+      return {
+        ...state, 
+        formState: action.newState
+      };
+    case SET_AUTH:
+      return {
+        ...state, 
+        loggedIn: action.newState
+      };
+    case SENDING_REQUEST:
+      return {
+        ...state,
+        currentlySending: action.sending
+      };
+    case SET_ERROR_MESSAGE:
+      return {
+        ...state,
+        errorMessage: action.message
+      };
+    default:
+      return state;
+  }
+}
